59ec06f7a7fb6b4e9cb7202d05ba4353a527848c
Atchukorn Tonsiriskulvong <[tatchukorn@gmail.com]>
2021-03-23 23:01:53 +0700

    text

---
+++ src/app/lab4/lab4.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lab4',
+  templateUrl: './lab4.component.html',
+  styleUrls: ['./lab4.component.css']
+})
+export class Lab4Component {
+
+  title1 = 'ngFor, ngClass ';
+  arr1 : number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+}
+
